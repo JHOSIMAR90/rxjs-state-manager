1fc3b30938703cc733c3c3182e235558026281e3
unknown <jhosimar>
2024-04-14 23:40:55 -0500

    IDL3 - Ingenieria Web

---
+++ src/app/data/products.ts
@@ -0,0 +1,85 @@
+export const Products = {
+  tecnology: [
+    {
+      id: 10,
+      name: 'Asus Vivo AIO',
+      description: '16GB RAM 512GB SSD',
+      price: 2799,
+      image: 'assets/products/images/all-in-one-asus-vivo-aio-r5-7520u-512gb-ssd-8.jpg'
+    },
+    {
+      id: 22,
+      name: 'Asus Zenbook OLED',
+      description: 'Ryzen 7 16GB RAM 512GB',
+      price: 3649,
+      image: 'assets/products/images/laptop-asus-zenbook-14-oled-um3402.jpg'
+    },
+    {
+      id: 34,
+      name: 'Asus ROG STRIX',
+      description: 'Ryzen 9 16GB RAM 1TB',
+      price: 8299,
+      image: 'assets/products/images/laptop-gamer-asus-rog-strix-g17-g713rw.jpg'
+    },
+    {
+      id: 41,
+      name: 'Asus ROG Zephyrus',
+      description: 'Ryzen 7 16GB RAM 512GB',
+      price: 5099,
+      image: 'assets/products/images/laptop-gamer-asus-rog-zephyrus-g14.jpg'
+    },
+    {
+      id: 59,
+      name: 'Asus TUF Dash F15',
+      description: 'Core i5 8GB RAM 512GB',
+      price: 3599,
+      image: 'assets/products/images/laptop-gamer-asus-tuf-dash-f15-fx517zc.jpg'
+    },
+    {
+      id: 6,
+      name: 'Lenovo Legión 5i',
+      description: 'i7 16GB RAM 512GB',
+      price: 6799,
+      image: 'assets/products/images/laptop-gamer-lenovo-legion-5i-intel-core-i7.jpg'
+    },
+    {
+      id: 23,
+      name: 'LG Gram 17Z90Q',
+      description: 'i7 16GB RAM 512GB SSD',
+      price: 6799,
+      image: 'assets/products/images/laptop-lg-gram-17z90q-g.ah76-intel-evo.jpg'
+    },
+  ],
+
+  textile: [
+    {
+      id: 69,
+      name: 'Motorola Moto G24',
+      description: '6.6" 4GB 256 GB Lavanda',
+      price: 549.00,
+      image: 'assets/products/images/celular-motorola-moto-g24-6.6-4gb-ram-256.jpg'
+    },
+    {
+      id: 65,
+      name: 'Galaxy A55 5G',
+      description: '6.6" 256GB 8GB Violet',
+      price: 1899,
+      image: 'assets/products/images/celular-samsung-galaxy-a55-5g-6.6.jpg'
+    },
+    {
+      id: 81,
+      name: 'Galaxy S23 Ultra',
+      description: '6.8" 12GB 512GB Black',
+      price: 4999,
+      image: 'assets/products/images/celular-samsung-galaxy-s23-ultra-6.8.jpg'
+    },
+    {
+      id: 124,
+      name: 'Redmi Note 13 Pro',
+      description: '6.67” 8GB 256GB Midnight',
+      price: 1249,
+      image: 'assets/products/images/celular-xiaomi-redmi-note-13-pro-6.67.jpg'
+    },
+  ]
+
+}
